d37f1e5e3972762f8ba22946f91d4816a92a7160
Yusuf Can Saglam <yusufcan.sglm@icloud.com>
2023-06-05 22:10:59 +0300

    Initial Commit

---
+++ SeanAllenWeather/SeanAllenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllenWeatherApp.swift
+//  SeanAllenWeather
+//
+//  Created by Yusuf Can Sağlam on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
